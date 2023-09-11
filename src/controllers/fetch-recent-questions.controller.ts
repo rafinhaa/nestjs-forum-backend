2ab73cd32ae5c22060f5f42a04f1eca8d5264bf0
Rafinhaa <rafinhaa@me.com>
2023-09-11 19:49:20 -0300

    Fundamentos do Nest - Controller de listagem de perguntas

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { ZodValidatePipe } from "src/pipes/zod-validate-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const fetchRecentQuestionsParamsSchema = z.object({
+  page: z.coerce.number().optional().default(1),
+  limit: z.coerce.number().optional().default(10),
+});
+
+const paramsValidationPipe = new ZodValidatePipe(
+  fetchRecentQuestionsParamsSchema
+);
+
+type FetchRecentQuestionsParamsSchema = z.infer<
+  typeof fetchRecentQuestionsParamsSchema
+>;
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(
+    @Query(paramsValidationPipe) params: FetchRecentQuestionsParamsSchema
+  ) {
+    const pages = Math.ceil(
+      (await this.prisma.question.count()) / params.limit
+    );
+
+    const questions = await this.prisma.question.findMany({
+      take: params.limit,
+      skip: (params.page - 1) * params.limit,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return {
+      pages,
+      questions,
+    };
+  }
+}
--- src/pipes/zod-validate-pipe.ts
+++ src/pipes/zod-validate-pipe.ts
@@ -2,11 +2,21 @@ import { BadRequestException, HttpStatus, PipeTransform } from "@nestjs/common";
 import { ZodError, ZodSchema } from "zod";
 
 export class ZodValidatePipe implements PipeTransform {
+  private value: unknown;
+
   constructor(private schema: ZodSchema) {}
+
+  getValue() {
+    return this.value;
+  }
+
+  setValue(value: unknown) {
+    this.value = value;
+  }
 
   transform(value: unknown) {
     try {
-      this.schema.parse(value);
+      this.setValue(this.schema.parse(value));
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({
@@ -17,6 +27,6 @@ export class ZodValidatePipe implements PipeTransform {
       }
       throw new BadRequestException();
     }
-    return value;
+    return this.getValue();
   }
 }
